c070e75f2c0ecc68b161fdfe056f45912fc5b94b
TinaMuuto <145022549+TinaMuuto@users.noreply.github.com>
2025-07-10 14:34:55 +0200

    Update cloudinary_upload.py

--- cloudinary_upload.py
+++ cloudinary_upload.py
@@ -1,5 +1,9 @@
+import os
 import cloudinary
 import cloudinary.uploader
+
+# DEBUG: Print Cloudinary URL env var
+print("DEBUG: CLOUDINARY_URL =", os.environ.get("CLOUDINARY_URL"))
 
 def upload_to_cloudinary(file_path):
     result = cloudinary.uploader.upload(file_path)
