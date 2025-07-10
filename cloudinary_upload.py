fb15525cef75d274832c0f6b519ef441fc1d1d1e
TinaMuuto <145022549+TinaMuuto@users.noreply.github.com>
2025-07-10 13:52:54 +0200

    Update cloudinary_upload.py

--- cloudinary_upload.py
+++ cloudinary_upload.py
@@ -1,15 +1,8 @@
 import cloudinary
 import cloudinary.uploader
-import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
-cloudinary.config(
-  cloud_name = os.environ.get("CLOUDINARY_CLOUD_NAME"),
-  api_key = os.environ.get("CLOUDINARY_API_KEY"),
-  api_secret = os.environ.get("CLOUDINARY_API_SECRET")
-)
+load_dotenv()  # Læser .env-filen (CLOUDINARY_URL)
 
 def upload_to_cloudinary(file_path):
     result = cloudinary.uploader.upload(file_path)
