0ab0b01df44fa9f3e8528f4cf084b851872edcee
TinaMuuto <145022549+TinaMuuto@users.noreply.github.com>
2025-07-10 14:23:17 +0200

    Update cloudinary_upload.py

--- cloudinary_upload.py
+++ cloudinary_upload.py
@@ -1,8 +1,5 @@
 import cloudinary
 import cloudinary.uploader
-from dotenv import load_dotenv
-
-load_dotenv()  # Læser .env-filen (CLOUDINARY_URL)
 
 def upload_to_cloudinary(file_path):
     result = cloudinary.uploader.upload(file_path)
